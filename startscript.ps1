abd0d018baf60c0280a72f9b3f3ea03fe20a7316
vijaidjearam <vijai.djearam@gmail.com>
2020-01-16 11:49:55 +0100

    Update startscript.ps1

--- startscript.ps1
+++ startscript.ps1
@@ -1,4 +1,12 @@
 Add-Type -AssemblyName PresentationFramework
 [System.Windows.MessageBox]::Show('Hello')
 #Installing Chocolatey
+try
+{
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+[System.Windows.MessageBox]::Show('Chocolatey Installed Correctly')
+}
+catch
+{
+[System.Windows.MessageBox]::Show('Problem Encountered during the installation of Chocolatey')
+}
