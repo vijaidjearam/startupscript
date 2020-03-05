d2f09c26b2414d877bd92e0d849c8f6ba90c3454
vijaidjearam <vijai.djearam@gmail.com>
2020-03-05 15:41:45 +0100

    Update logonscript.ps1

--- logonscript.ps1
+++ logonscript.ps1
@@ -1,15 +1,16 @@
-Add-Type -AssemblyName PresentationFramework
-[System.Windows.MessageBox]::Show('Hello')
+#Add-Type -AssemblyName PresentationFramework
+#[System.Windows.MessageBox]::Show('Hello')
 #Installing Chocolatey
 try
 {
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) |Out-Null
 write-host " Chocolatey Install --------------✔"
-[System.Windows.MessageBox]::Show('Chocolatey Installed Correctly')
+#[System.Windows.MessageBox]::Show('Chocolatey Installed Correctly')
 }
 catch
 {
-[System.Windows.MessageBox]::Show('Problem Encountered during the installation of Chocolatey')
+write-host "Problem encountered while installing chocolatey" 
+#[System.Windows.MessageBox]::Show('Problem Encountered during the installation of Chocolatey')
 }
 
 #Configuring internal chocolatey server
