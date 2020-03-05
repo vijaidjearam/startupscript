187206630ee9c39bd33cdaa43957b1f7af41533b
vijaidjearam <vijai.djearam@gmail.com>
2020-03-05 10:00:51 +0100

    added chocolatey packages

--- logonscript.ps1
+++ logonscript.ps1
@@ -10,3 +10,33 @@ catch
 {
 [System.Windows.MessageBox]::Show('Problem Encountered during the installation of Chocolatey')
 }
+
+#Configuring internal chocolatey server
+choco source add -n chocosia -s "http://choco.iut-troyes.univ-reims.fr/chocolatey" --priority=1
+choco source add -n chocolatey -s "https://chocolatey.org/api/v2" --priority=2
+
+#The packages that are not necessary can be commented with #
+$chocoapps = @(
+    ".7zip.install",
+    "adobeshockwaveplayer",
+    "ccleaner",
+    "dellcommandupdate", 
+    "filezilla", 
+    "firefox", 
+    "flashplayeractivex",
+    "flashplayerplugin", 
+    "foxitreader", 
+    "gimp", 
+    "git", 
+    "googlechrome",
+    "libreoffice", 
+    "pdfcreator", 
+    "quicktime",
+    "sublimetext3",
+    "totalcommander",
+    "vlc",
+    "vscode" 
+    # "aimp",
+)
+cinst -s chocosia -y Powershell 
+cinst -s chocosia -y $chocoapps
