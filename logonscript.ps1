2cad1623e4bf7fa7af2e0d83625977338f67aace
vijaidjearam <vijai.djearam@gmail.com>
2020-03-05 12:28:15 +0100

    Update logonscript.ps1

--- logonscript.ps1
+++ logonscript.ps1
@@ -4,6 +4,7 @@ Add-Type -AssemblyName PresentationFramework
 try
 {
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) |Out-Null
+write-host " Chocolatey Install --------------✔"
 [System.Windows.MessageBox]::Show('Chocolatey Installed Correctly')
 }
 catch
@@ -13,6 +14,7 @@ catch
 
 #Configuring internal chocolatey server
 choco source add -n chocosia -s "http://choco.iut-troyes.univ-reims.fr/chocolatey" --priority=1 |Out-Null
+write-host " Internal chocolatey configured --------------✔"
 choco source add -n chocolatey -s "https://chocolatey.org/api/v2" --priority=2 |Out-Null
 
 #The packages that are not necessary can be commented with #
@@ -42,6 +44,8 @@ cinst -s chocosia -y Powershell |Out-Null
 cinst -s chocolatey -y dotnet4.6.1 |Out-Null # dependency needed for dell command update
 cinst -s chocolatey -y dotnetfx |Out-Null
 cinst -s chocosia -y --ignore-checksums $chocoapps |Out-Null
+write-host "Packeges installed via chocolatey --------------✔"
+
 #pause the script at the end to see the status.
 Pause
 
