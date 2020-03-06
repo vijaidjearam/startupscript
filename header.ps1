c0c20c85ae98b76f4f499e72b273e03241c400e3
vijaidjearam <vijai.djearam@gmail.com>
2020-03-06 09:16:58 +0100

    Update header.ps1

--- header.ps1
+++ header.ps1
@@ -1,5 +1,6 @@
 write-host " Installing Chocolatey"
 iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1")) | Out-Null
+write-host " Chocolatey Installed Successfully --------------✔"
 choco source add -n chocosia -s "http://choco.iut-troyes.univ-reims.fr/chocolatey" --priority=1 | Out-Null
 write-host " Internal chocolatey configured --------------✔"
 choco source add -n chocolatey -s "https://chocolatey.org/api/v2" --priority=2 | Out-Null
