88c86cf056fe6fde44deca4a958ec12d0b4ebda9
vijaidjearam <vijai.djearam@gmail.com>
2020-03-06 16:18:48 +0100

    Update header.ps1

--- header.ps1
+++ header.ps1
@@ -1,16 +1,16 @@
 $WarningPreference = 'SilentlyContinue'
 write-host " Installing Chocolatey"
 iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1")) | Out-Null
-write-host "Chocolatey Installed Successfully --------------✔"
+write-host "Chocolatey Installed Successfully --------------Ok"
 choco source add -n chocosia -s "http://choco.iut-troyes.univ-reims.fr/chocolatey" --priority=1 | Out-Null
-write-host "Internal chocolatey configured --------------✔"
+write-host "Internal chocolatey configured --------------Ok"
 choco source add -n chocolatey -s "https://chocolatey.org/api/v2" --priority=2 | Out-Null
-write-host "chocolatey by default has been configured to priority 2 --------------✔"
+write-host "chocolatey by default has been configured to priority 2 --------------Ok"
 #Stage 1 - Installing chocolatey Apps
 try{
 iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/vijaidjearam/startupscript/master/chocolatey_packages.ps1")) | Out-Null
-write-host "Stage 1 Installing chocolatey Package --------------✔"
+write-host "Stage 1 Installing chocolatey Package --------------Ok"
 }
-catch{write-host "Error Occured in Stage 1 Installing chocolatey Package--------------❌"}
+catch{write-host "Error Occured in Stage 1 Installing chocolatey Package--------------NOK"-ForeGroundColor Red}
 Pause
 
