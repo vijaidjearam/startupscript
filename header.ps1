2945de22caf975c0c2b538af499a0fc251c221fa
vijaidjearam <vijai.djearam@gmail.com>
2020-03-06 16:03:39 +0100

    Update header.ps1

--- header.ps1
+++ header.ps1
@@ -1,47 +1,16 @@
 $WarningPreference = 'SilentlyContinue'
 write-host " Installing Chocolatey"
 iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1")) | Out-Null
-write-host "Chocolatey Installed Successfully --------------OK"
+write-host "Chocolatey Installed Successfully --------------✔"
 choco source add -n chocosia -s "http://choco.iut-troyes.univ-reims.fr/chocolatey" --priority=1 | Out-Null
-write-host "Internal chocolatey configured --------------Ok"
+write-host "Internal chocolatey configured --------------✔"
 choco source add -n chocolatey -s "https://chocolatey.org/api/v2" --priority=2 | Out-Null
-write-host "chocolatey by default has been configured to priority 2 --------------Ok"
-$chocoapps = @(
-    	"7zip.install",
-        "dotnetfx"
-    	"adobeshockwaveplayer",
-    	"ccleaner",
-    	"dellcommandupdate", 
-    	"filezilla", 
-    	"firefox", 
-    	"flashplayeractivex",
-    	"flashplayerplugin", 
-    	"foxitreader", 
-    	"gimp", 
-    	"git", 
-    	"googlechrome",
-    	"libreoffice-fresh", 
-    	"pdfcreator", 
-    	"quicktime",
-    	"sublimetext3",
-    	"totalcommander",
-    	"vlc",
-    	"vscode" 
-    	# "aimp",
-)
-cinst -s chocosia -y Powershell | Out-Null 
-#dependency for dell command update
-cinst -s chocolatey -y dotnet4.6.1 | Out-Null 
-cinst -s chocolatey -y dotnetfx | Out-Null
-$counter = 0
-foreach($item in $chocoapps){
-    $counter++
-    Write-Progress -Activity 'Install Apps' -CurrentOperation $item -PercentComplete (($counter / $chocoapps.count) * 100)
-    Start-Sleep -Milliseconds 200
-    cinst -s chocosia -y --ignore-checksums $item | Out-Null
-    if($LASTEXITCODE -eq 0){write-host $item"--------"Ok}
-    else{write-host $item"-----------Nok" -ForegroundColor Red}
+write-host "chocolatey by default has been configured to priority 2 --------------✔"
+#Stage 1 - Installing chocolatey Apps
+try{
+iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/vijaidjearam/startupscript/master/chocolatey_packages.ps1")) | Out-Null
+write-host "Stage 1 Installing chocolatey Package --------------✔"
 }
-
+catch{write-host "Error Occured in Stage 1 Installing chocolatey Package--------------❌"}
 Pause
 
