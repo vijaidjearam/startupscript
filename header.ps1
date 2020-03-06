441dd6c7dad369e499cf5135019e06e6b6757f27
vijaidjearam <vijai.djearam@gmail.com>
2020-03-06 08:57:53 +0100

    Update header.ps1

--- header.ps1
+++ header.ps1
@@ -1,10 +1,11 @@
-iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"))
-choco source add -n chocosia -s "http://choco.iut-troyes.univ-reims.fr/chocolatey" --priority=1
+iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1")) | Out-Null
+choco source add -n chocosia -s "http://choco.iut-troyes.univ-reims.fr/chocolatey" --priority=1 | Out-Null
 write-host " Internal chocolatey configured --------------✔"
-choco source add -n chocolatey -s "https://chocolatey.org/api/v2" --priority=2
+choco source add -n chocolatey -s "https://chocolatey.org/api/v2" --priority=2 | Out-Null
 write-host " chocolatey by default has been configured to priority 2 --------------✔"
 $chocoapps = @(
     	"7zip.install",
+        "dotnetfx"
     	"adobeshockwaveplayer",
     	"ccleaner",
     	"dellcommandupdate", 
@@ -25,9 +26,18 @@ $chocoapps = @(
     	"vscode" 
     	# "aimp",
 )
-cinst -s chocosia -y Powershell 
-cinst -s chocolatey -y dotnet4.6.1 
-cinst -s chocolatey -y dotnetfx
-cisnt -s chocosia -y --ignore-checksums $chocoapps
+cinst -s chocosia -y Powershell | Out-Null 
+#dependency for dell command update
+cinst -s chocolatey -y dotnet4.6.1 | Out-Null 
+cinst -s chocolatey -y dotnetfx | Out-Null
+$counter = 0
+foreach($item in $chocoapps){
+    $counter++
+    Write-Progress -Activity 'Install Apps' -CurrentOperation $item -PercentComplete (($counter / $chocoapps.count) * 100)
+    Start-Sleep -Milliseconds 200
+    cinst -s chocosia -y --ignore-checksums $item
+    if($LASTEXITCODE -eq 0){write-host "$item--------✔"}
+    else{write-host "$item-----------❌"}
+}
 Pause
 
