1f848c4b7bcac07aca1f4733ede3c342482df1b3
vijaidjearam <vijai.djearam@gmail.com>
2020-03-11 15:34:40 +0100

    Update header.ps1

--- header.ps1
+++ header.ps1
@@ -15,17 +15,12 @@ write-host "End Of Stage 1 -Installed chocolatey Package --------------Ok"
 catch{write-host "Error Occured in Stage 1 Installing chocolatey Package--------------NOK"-ForeGroundColor Red}
 #Stage 2 - Configuring Windows Services------------------------------------------------------------------------------------------------
 write-host "Entering - Stage 2 : Configuring Windows Services --------------Ok"
-try{
 iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/vijaidjearam/startupscript/master/windows_services.ps1")) | Out-Null
 write-host "End Of Stage 2 - Configured Windows Services --------------Ok"
-}
-catch{write-host "Error Occured in Stage 2 Configuring Windows Services--------------NOK"-ForeGroundColor Red}
+
 #Stage 3 - Configuring Windows Settings --------------------------------------------------------------------------------------------------
 write-host "Entering - Stage 3 : Configuring Windows Settings --------------Ok"
-try{
 iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/vijaidjearam/startupscript/master/windows_settings.ps1")) | Out-Null
 write-host "End Of Stage 3 - Configured Windows Settings --------------Ok"
-}
-catch{write-host "Error Occured in Stage 3 :Configuring Windows Settings--------------NOK"-ForeGroundColor Red}
-Pause
+
 
