78da610c3aa0a1431c5d5aa3ae7c484728832d5f
vijaidjearam <vijai.djearam@gmail.com>
2020-03-12 16:42:43 +0100

    Update chocolatey_packages.ps1

--- chocolatey_packages.ps1
+++ chocolatey_packages.ps1
@@ -33,5 +33,10 @@ foreach($item in $chocoapps){
     Start-Sleep -Milliseconds 200
     cinst -s chocosia -y --ignore-checksums $item | Out-Null
     if($LASTEXITCODE -eq 0){write-host $item"--------"Ok}
-    else{write-host $item"-----------Nok" -ForegroundColor Red}
+    else{write-host $item"--failed installing with internal server----------Nok" -ForegroundColor Red
+    write-host "Trying to install with Chocolatey sever" -ForeGroundColor Yellow
+    cinst -s chocolatey -y --ignore-checksums $item | Out-Null
+    if($LASTEXITCODE -eq 0){write-host $item"--------"Ok}
+    else{write-host $item"-failed installing even with chocolatey server-----------Nok" -ForegroundColor Red}
+    }
 }
