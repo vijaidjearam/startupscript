10acb614bc5c54c1ba9e869685bcf7004d2ede2e
vijaidjearam <vijai.djearam@gmail.com>
2020-03-12 11:00:37 +0100

    Update header.ps1

--- header.ps1
+++ header.ps1
@@ -7,9 +7,11 @@ write-host "Internal chocolatey configured --------------Ok"
 choco source add -n chocolatey -s "https://chocolatey.org/api/v2" --priority=2 | Out-Null
 write-host "chocolatey by default has been configured to priority 2 --------------Ok"
 #Stage 1 - Installing chocolatey Apps-------------------------------------------------------------------------------------------------
-chocolatey_apps
-windows_service
-windows_settings
+$stages =@( 
+"chocolatey_apps",
+"windows_service",
+"windows_settings"
+)
 function chocolatey_apps{
   #Stage 1 - Installing chocolatey Apps-------------------------------------------------------------------------------------------------
   write-host "Entering - Stage 1 : Installing chocolatey Package --------------Ok"
@@ -28,4 +30,10 @@ write-host "Entering - Stage 3 : Configuring Windows Settings --------------Ok"
 iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/vijaidjearam/startupscript/master/windows_settings.ps1")) | Out-Null
 write-host "End Of Stage 3 - Configured Windows Settings --------------Ok"
 }
-
+$counter = 1
+foreach($item in $chocoapps){
+    write-host "stage - "$counter
+    $item
+    write-host "End of stage -"$counter
+    $counter++    
+}
