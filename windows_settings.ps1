41ce679fa489626dcfe2698c43d70803cd67e08f
vijaidjearam <vijai.djearam@gmail.com>
2020-03-12 15:13:44 +0100

    Update windows_settings.ps1

--- windows_settings.ps1
+++ windows_settings.ps1
@@ -1,12 +1,23 @@
 $WarningPreference = 'SilentlyContinue'
 iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/vijaidjearam/startupscript/master/windows_settings_essentials.ps1"))
 write-host "Entering Windows-settings Configuration Stage" 
-Set-TaskbarOptions -Size Small
-Enable-RemoteDesktop
-set-ntpserver_urca
-open_ports
-Set-ExplorerOptions -showHiddenFilesFoldersDrives
-Set-ExplorerOptions -showProtectedOSFiles
-Set-ExplorerOptions -showFileExtensions
-AllowInsecureGuestAuth #Allow guest access to SAMBA share.
+$setting = @(
+"Set-TaskbarOptions -Size Small",
+"Enable-RemoteDesktop",
+"set-ntpserver_urca",
+"open_ports",
+"Show-HiddenFile",
+"Show-FileExtensions",
+"AllowInsecureGuestAuth" #Allow guest access to SAMBA share.
+)
 
+$setting | foreach {
+try{
+Invoke-Expression $_ |Out-Null
+write-host $_ "---------------------OK" -ForegroundColor Green
+}
+catch{
+write-host  ""
+write-host $_ "--------------Nok" -ForegroundColor Red
+}
+}
