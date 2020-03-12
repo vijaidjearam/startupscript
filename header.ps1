52d1ebbfa03e5b0656f2660f373ddf32cb6245c3
vijaidjearam <vijai.djearam@gmail.com>
2020-03-12 12:17:27 +0100

    Update header.ps1

--- header.ps1
+++ header.ps1
@@ -1,4 +1,6 @@
 $WarningPreference = 'SilentlyContinue'
+$FileName = $env:TEMP+"\"+(Get-Date).tostring("dd-MM-yyyy-hh-mm-ss") + "_transcript.txt"
+Start-Transcript -path $FileName -NoClobber
 write-host " Installing Chocolatey"
 iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1")) | Out-Null
 write-host "Chocolatey Installed Successfully --------------Ok"
@@ -31,4 +33,5 @@ iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercon
 write-host "End Of Stage 3 - Configured Windows Settings --------------Ok"
 }
 $stages | ForEach { Invoke-Expression $_ }
+Stop-Transcript
 Pause
