57b8941233a1e61e7f09540b36a07a988faae44e
vijaidjearam <vijai.djearam@gmail.com>
2020-03-12 14:58:22 +0100

    Update windows_settings_essentials.ps1

--- windows_settings_essentials.ps1
+++ windows_settings_essentials.ps1
@@ -13,7 +13,6 @@ function Show-HiddenFile
 } 
 function Show-FileExtensions
 {
-    # http://superuser.com/questions/666891/script-to-set-hide-file-extensions
     Push-Location
     Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
     Set-ItemProperty . HideFileExt "0"
