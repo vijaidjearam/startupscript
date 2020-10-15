86afa67f7daf979b97a3196f863fd5e1a7c55ac4
vijai <vijai.djearam@gmail.com>
2020-10-15 17:07:50 +0200

    Update windows_settings.ps1

--- windows_settings.ps1
+++ windows_settings.ps1
@@ -2,13 +2,20 @@ $WarningPreference = 'SilentlyContinue'
 iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/vijaidjearam/startupscript/master/windows_settings_essentials.ps1"))
 write-host "Entering Windows-settings Configuration Stage" 
 $setting = @(
-"Set-TaskbarOptions -Size Small",
+"Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives",
+"Set-WindowsExplorerOptions -EnableShowProtectedOSFiles",
+"Set-WindowsExplorerOptions -EnableShowFileExtensions",
+"Set-WindowsExplorerOptions -EnableShowFullPathInTitleBar",
+"Set-WindowsExplorerOptions -DisableOpenFileExplorerToQuickAccess",
+"Set-WindowsExplorerOptions -DisableShowRecentFilesInQuickAccess",
+"Set-WindowsExplorerOptions -DisableShowFrequentFoldersInQuickAccess",
+"Set-WindowsExplorerOptions -EnableSnapAssist",
+"Set-TaskbarOptions -Lock -NoAutoHide -Size Small",
 "set-desktop-icon-small",
 "Enable-RemoteDesktop",
 "set-ntpserver_urca",
 "open_ports",
-"Show-HiddenFile",
-"Show-FileExtensions",
+
 'Disable-ComputerRestore -Drive "c:\"',
 "Set-Wsus",
 "AllowInsecureGuestAuth" #Allow guest access to SAMBA share.
