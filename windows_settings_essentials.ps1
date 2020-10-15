7b3ce2bfe5e0884bff440c4de60ec237f663826c
vijai <vijai.djearam@gmail.com>
2020-10-15 17:05:48 +0200

    Update windows_settings_essentials.ps1

--- windows_settings_essentials.ps1
+++ windows_settings_essentials.ps1
@@ -1,3 +1,4 @@
+<#
 function Show-HiddenFile
 {
     param([Switch]$Off)
@@ -19,6 +20,7 @@ function Show-FileExtensions
     Pop-Location
     Stop-Process -processName: Explorer -force # This will restart the Explorer service to make this work.
 }
+#>
 
 function Enable-RemoteDesktop {
 <#
@@ -456,33 +458,7 @@ https://boxstarter.org
 
     Restart-Explorer
 }
-function Set-ExplorerOptions {
-<#
-.SYNOPSIS
-Sets options on the windows Explorer shell
-.PARAMETER showHiddenFilesFoldersDrives
-If this switch is set, hidden files will be shown in windows explorer
-.PARAMETER showProtectedOSFiles
-If this flag is set, hidden Operating System files will be shown in windows explorer
-.PARAMETER showFileExtensions
-Setting this switch will cause windows explorer to include the file extension in file names
-.LINK
-https://boxstarter.org
-#>
-    param(
-        [alias("showHidenFilesFoldersDrives")]
-        [switch]$showHiddenFilesFoldersDrives,
-        [switch]$showProtectedOSFiles,
-        [switch]$showFileExtensions
-    )
 
-	Write-Warning "This command is deprecated, use Set-WindowsExplorerOptions instead."
-	Write-Warning "Your call to this function will now be routed to the Set-WindowsExplorerOptions function."
-
-	if($showHiddenFilesFoldersDrives) { Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives }
-    if($showFileExtensions) { Set-WindowsExplorerOptions -EnableShowFileExtensions }
-    if($showProtectedOSFiles) { Set-WindowsExplorerOptions -EnableShowProtectedOSFiles }
-}
 function AllowInsecureGuestAuth{
 	Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters" -Name "AllowInsecureGuestAuth" -Value 1
 }
