52bdc800a9d30340f1f42e44b66fdc446e117d42
vijai <vijai.djearam@gmail.com>
2020-10-16 15:34:30 +0200

    Add files via upload

---
+++ provisioning-win-edu.bat
@@ -0,0 +1,12 @@
+dism /Export-Image /SourceImageFile:%~dp0sources\install.esd /SourceIndex:4 /DestinationImageFile:%temp%\install.wim /Compress:Max /CheckIntegrity
+set workdir=c:\temp\%date:~7,2%-%date:~-10,2%-%date:~-2,2%_%time:~0,2%.%time:~3,2%.%time:~6,2%
+md %workdir%
+DISM /Mount-Wim /WimFile:%temp%\install.wim /index:1 /MountDir:%workdir%
+md %workdir%\Windows\Setup\Scripts
+echo reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" -v !test /t REG_SZ -d "cmd /k @powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command \" iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))\"" /f > %workdir%\Windows\Setup\Scripts\SetupComplete.cmd
+DISM /unmount-wim /MountDir:%workdir% /commit
+DEL %~dp0sources\install.esd
+robocopy %temp% %~dp0sources install.wim /MT:8
+DEL %temp%\install.wim
+DEL %workdir%
+pause
