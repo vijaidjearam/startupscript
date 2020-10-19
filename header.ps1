f18f1e1a8340ddaa94341d6bcf0812625eb48f24
vijai <vijai.djearam@gmail.com>
2020-10-19 12:49:59 +0200

    Update header.ps1

--- header.ps1
+++ header.ps1
@@ -26,27 +26,27 @@ function network{
 }
 function chocolatey_apps{
   #Stage 1 - Installing chocolatey Apps-------------------------------------------------------------------------------------------------
-  write-host "Entering - Stage 1 : Installing chocolatey Package --------------Ok"
+  write-host "Entering - Stage 2 : Installing chocolatey Package --------------Ok"
   iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/vijaidjearam/startupscript/master/chocolatey_packages.ps1")) | Out-Null
-  write-host "End Of Stage 1 -Installed chocolatey Package --------------Ok"
+  write-host "End Of Stage 2 -Installed chocolatey Package --------------Ok"
 }
 function windows_service{
 #Stage 2 - Configuring Windows Services------------------------------------------------------------------------------------------------
-write-host "Entering - Stage 2 : Configuring Windows Services --------------Ok"
+write-host "Entering - Stage 3 : Configuring Windows Services --------------Ok"
 iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/vijaidjearam/startupscript/master/windows_services.ps1")) | Out-Null
-write-host "End Of Stage 2 - Configured Windows Services --------------Ok"
+write-host "End Of Stage 3 - Configured Windows Services --------------Ok"
 }
 function windows_settings{
 #Stage 3 - Configuring Windows Settings --------------------------------------------------------------------------------------------------
-write-host "Entering - Stage 3 : Configuring Windows Settings --------------Ok"
+write-host "Entering - Stage 4 : Configuring Windows Settings --------------Ok"
 iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/vijaidjearam/startupscript/master/windows_settings.ps1")) | Out-Null
-write-host "End Of Stage 3 - Configured Windows Settings --------------Ok"
+write-host "End Of Stage 4 - Configured Windows Settings --------------Ok"
 }
 function cleaning{
 #Stage 3 - Cleaning files --------------------------------------------------------------------------------------------------
-write-host "Entering - Stage 4 : Cleaning files created during install --------------Ok"
+write-host "Entering - Stage 5 : Cleaning files created during install --------------Ok"
 iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/vijaidjearam/startupscript/master/cleaning.ps1")) | Out-Null
-write-host "End Of Stage 3 - Cleaning files completed --------------Ok"
+write-host "End Of Stage 5 - Cleaning files completed --------------Ok"
 }
 $stages | ForEach { Invoke-Expression $_ }
 Stop-Transcript
