640a4b008a4d14d45e52b7e085285ddba389e669
vijai <vijai.djearam@gmail.com>
2020-12-10 09:13:04 +0100

    Update cleaning.ps1

--- cleaning.ps1
+++ cleaning.ps1
@@ -1,1 +1,4 @@
 Remove-Item 'C:\Windows\Setup\Scripts\SetupComplete.cmd'
+Write-host "Cleaning Event Log" 
+Get-EventLog -LogName * | ForEach { Clear-EventLog $_.Log }
+Write-host "Completed Cleaning Event Log" 
