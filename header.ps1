e5d27316e6e71950cf04b3dea11183e2ecc5c638
vijai <vijai.djearam@gmail.com>
2021-02-05 16:15:24 +0100

    Update header.ps1

--- header.ps1
+++ header.ps1
@@ -11,8 +11,8 @@ choco source remove -n chocolatey
 write-host "chocolatey by default has been removed --------------Ok"
 #Stage 1 - Installing chocolatey Apps-------------------------------------------------------------------------------------------------
 $stages =@(
-"network",
-"chocolatey_apps",
+#"network",
+#"chocolatey_apps",
 "windows_service",
 "windows_settings",
 "cleaning"
