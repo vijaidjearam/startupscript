2e2fb838a7c06243615d5dce8cfb292e545a9c27
vijai <vijai.djearam@gmail.com>
2021-02-08 13:39:50 +0100

    Update windows_settings_essentials.ps1

--- windows_settings_essentials.ps1
+++ windows_settings_essentials.ps1
@@ -531,8 +531,8 @@ write-host $_ "---------------------OK" -ForegroundColor Green
 }
 
 }
-}
 catch{}
+}
 }
 #A titre d’exemple de simplification de la UI, aller dans les paramètres avancés,sélectionner « Ajuster pour obtenir les meilleures performances pour lesprogrammes » et cochez dans la liste dessous « Afficher des miniatures au lieu d’icônes », ainsi que « Lisser les polices d’écran ».
 function performance_options_visual_effects{
