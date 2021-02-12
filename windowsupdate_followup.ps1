989de1785073f21d428452ce3c2bdbd83664878b
vijai <vijai.djearam@gmail.com>
2021-02-12 16:26:10 +0100

    Update windowsupdate_followup.ps1

--- windowsupdate_followup.ps1
+++ windowsupdate_followup.ps1
@@ -74,7 +74,7 @@ Import-Module -Name PSWindowsUpdate
 Install-WindowsUpdate -AcceptAll
 
 if (Get-WURebootStatus -silent){
-	Set-Runonce -command "%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/windowsupdate_followup.ps1'))"
+	Set-Runonce -command "%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/windowsupdate_followup.ps1'))"
 	Restart-Computer
 }
 else{
