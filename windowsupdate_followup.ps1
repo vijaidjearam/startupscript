3b5980dc5e811d292c1e750299937448f40de8f7
vijai <vijai.djearam@gmail.com>
2021-02-12 15:18:09 +0100

    Update windowsupdate_followup.ps1

--- windowsupdate_followup.ps1
+++ windowsupdate_followup.ps1
@@ -24,6 +24,52 @@ New-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\A
 New-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU -Name UseWUServer -Value 1 -Force
 }
 
+function Set-RunOnce
+  <#
+      .SYNOPSIS
+      Sets a Runonce-Registry Key
+ 
+      .DESCRIPTION
+      Sets a Runonce-Key in the Computer-Registry. Every Program which will be added will run once at system startup.
+      This Command can be used to configure a computer at startup.
+ 
+      .EXAMPLE
+      Set-Runonce -command '%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -file c:\Scripts\start.ps1'
+      Sets a Key to run Powershell at startup and execute C:\Scripts\start.ps1
+ 
+      .NOTES
+      Author: Holger Voges
+      Version: 1.0
+      Date: 2018-08-17
+ 
+      .LINK
+      https://www.netz-weise-it.training/
+  #>
+{
+    [CmdletBinding()]
+    param
+    (
+        #The Name of the Registry Key in the Autorun-Key.
+        [string]
+        $KeyName = 'Run',
+
+        #Command to run
+        [string]
+        $Command = '%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -file c:\Scripts\run1.ps1'
+  
+    ) 
+
+    
+    if (-not ((Get-Item -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce).$KeyName ))
+    {
+        New-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name $KeyName -Value $Command -PropertyType ExpandString
+    }
+    else
+    {
+        Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name $KeyName -Value $Command -PropertyType ExpandString
+    }
+}
+
 Import-Module -Name PSWindowsUpdate
 Install-WindowsUpdate -AcceptAll
 
