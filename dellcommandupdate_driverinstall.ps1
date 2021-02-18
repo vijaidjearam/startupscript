f13415fa26e0b4b772cec99d80110a00400efafc
vijai <vijai.djearam@gmail.com>
2021-02-18 17:15:43 +0100

    Update dellcommandupdate_driverinstall.ps1

--- dellcommandupdate_driverinstall.ps1
+++ dellcommandupdate_driverinstall.ps1
@@ -1,15 +1,14 @@
-$ErrorActionPreference = 'Stop'
-try
-{
 choco install dellcommandupdate -y
 & "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" /driverInstall
+if($LASTEXITCODE -eq 0)
+{
 write-host "Stage: dellcommandupdate_driverinstall completed" -ForegroundColor Green
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'dellcommandupdate_applyupdates'
 Set-Runonce -command "%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))"
 Stop-Transcript
 Restart-Computer
-}
-catch
+} 
+else 
 {
 write-host "Stage: dellcommandupdate_driverinstall Failed" -ForegroundColor Red
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value dellcommandupdate_driverinstall
