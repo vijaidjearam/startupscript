6775af266f19139ec35b850dbcfe105e68b0f761
vijai <vijai.djearam@gmail.com>
2021-02-18 17:19:30 +0100

    Update dellcommandupdate_applyupdates.ps1

--- dellcommandupdate_applyupdates.ps1
+++ dellcommandupdate_applyupdates.ps1
@@ -1,18 +1,17 @@
-$ErrorActionPreference = Stop
-try
-{
 & "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" /applyUpdates
+
+if($LASTEXITCODE -eq 0)
+{
 write-host "Stage: dellcommandupdate_applyupdates completed" -ForegroundColor Green
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'dellcommandconfigure'
 Set-Runonce -command "%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))"
 Stop-Transcript
 Restart-Computer
-}
-catch
+} 
+else 
 {
 write-host "Stage: $stage Failed" -ForegroundColor Red
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value dellcommandupdate_applyupdates
 Set-Runonce -command "%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))"
 Stop-Transcript
 }
-
