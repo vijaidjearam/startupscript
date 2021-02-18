c5e732f5d32ce459e40e98654fa426817815a361
vijai <vijai.djearam@gmail.com>
2021-02-18 16:12:52 +0100

    Update cleaning.ps1

--- cleaning.ps1
+++ cleaning.ps1
@@ -1,6 +1,18 @@
+try
+{
 if (test-path C:\Windows\Setup\Scripts\SetupComplete.cmd){Remove-Item 'C:\Windows\Setup\Scripts\SetupComplete.cmd'}
 Remove-Item -Path HKCU:\osinstall_local
 Write-host "Cleaning Event Log" 
-Try {Get-EventLog -LogName * | ForEach { Clear-EventLog $_.Log }}
-catch {write-host "Error while cleaning log files" -ForegroundColor Red}
+Get-EventLog -LogName * | ForEach { Clear-EventLog $_.Log }
 Write-host "Completed Cleaning Event Log" 
+write-host "Stage: cleaning completed" -ForegroundColor Green
+Stop-Transcript
+}
+catch
+{
+write-host "Stage: cleaning Failed" -ForegroundColor Red
+Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value cleaning
+Set-Runonce -command "%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))"
+Stop-Transcript
+
+}
