7c0bf1f15a607b794ce3cab766663a8ab9ce96d5
vijai <vijai.djearam@gmail.com>
2021-02-18 16:02:51 +0100

    Update windowsupdate_initiate.ps1

--- windowsupdate_initiate.ps1
+++ windowsupdate_initiate.ps1
@@ -1,10 +1,10 @@
 Try
 {
-write-host "Stage: windowsupdate_initiate completed" -ForegroundColor Green
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSWindowsUpdate -Force
 Import-Module -Name PSWindowsUpdate
 Install-WindowsUpdate -AcceptAll -IgnoreReboot
+write-host "Stage: windowsupdate_initiate completed" -ForegroundColor Green
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'windowsupdate_followup'
 Set-Runonce -command "%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))"
 Stop-Transcript
