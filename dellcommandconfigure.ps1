d5010497bbbeb0ee365f2bdd4deacee0cd0e60a3
vijai <vijai.djearam@gmail.com>
2021-02-19 12:12:07 +0100

    Update dellcommandconfigure.ps1

--- dellcommandconfigure.ps1
+++ dellcommandconfigure.ps1
@@ -112,10 +112,17 @@ write-host "$item is not available or cannot be configured " -ForegroundColor Re
 elseif($LASTEXITCODE -eq 58 -or $LASTEXITCODE -eq 65 -or $LASTEXITCODE -eq 66 -or $LASTEXITCODE -eq 67 -or $LASTEXITCODE -eq 109)
 {
 write-host "Password is set in the BIOS, please clear the BIOS password during restart and continue" -ForegroundColor Red
+$biospassword = Read-Host -Prompt 'Enter bios Password to clear it in bios'
+& "C:\Program Files (x86)\Dell\Command Configure\X86_64\cctk.exe" --syspwd=   --valsyspwd=$biospassword
+& "C:\Program Files (x86)\Dell\Command Configure\X86_64\cctk.exe" --$item | Out-Null
+write-host "setting $item was successfull" -ForegroundColor Green
+}
+else{
+write-host "Stage: dellcommandconfigure Failed" -ForegroundColor Red
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'dellcommandconfigure'
 Set-Runonce -command "%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass ; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))"
 Stop-Transcript
-Break
+Exit
 }
 }
 write-host "Stage: dellcommandconfigure completed" -ForegroundColor Green
