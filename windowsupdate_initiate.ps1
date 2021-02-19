0c3717be41c9c03089f71ddf7f3c7780918d1a18
vijai <vijai.djearam@gmail.com>
2021-02-19 15:29:19 +0100

    Update windowsupdate_initiate.ps1

--- windowsupdate_initiate.ps1
+++ windowsupdate_initiate.ps1
@@ -6,16 +6,15 @@ Import-Module -Name PSWindowsUpdate
 Install-WindowsUpdate -AcceptAll -IgnoreReboot
 write-host "Stage: windowsupdate_initiate completed" -ForegroundColor Green
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'windowsupdate_followup'
-Set-Runonce -command "%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass ; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))"
+Set-Runonce
 Stop-Transcript
-Pause
 Restart-Computer
 }
 catch
 {
 write-host "Stage: windowsupdate_initiate Failed" -ForegroundColor Red
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value windowsupdate_initiate
-Set-Runonce -command "%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass ; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))"
+Set-Runonce
 Stop-Transcript
 Pause
 }
