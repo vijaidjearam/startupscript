9b4386fd572e0cb03e50325421ce351fd2b4d08e
vijai <vijai.djearam@gmail.com>
2021-02-19 14:40:37 +0100

    Update dellcommandupdate_driverinstall.ps1

--- dellcommandupdate_driverinstall.ps1
+++ dellcommandupdate_driverinstall.ps1
@@ -1,6 +1,6 @@
 choco install dellcommandupdate -y
 & "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" /driverInstall
-if($LASTEXITCODE -eq 0 -or $LASTEXITCODE -eq 1 -or $LASTEXITCODE -eq 5)
+if($LASTEXITCODE -eq 0 -or $LASTEXITCODE -eq 1 -or $LASTEXITCODE -eq 5 -or $LASTEXITCODE -eq 500)
 {
 write-host "Stage: dellcommandupdate_driverinstall completed" -ForegroundColor Green
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'dellcommandupdate_applyupdates'
