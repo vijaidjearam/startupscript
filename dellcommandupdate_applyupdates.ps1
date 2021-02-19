09ca363ca8e1149006b0467663403f550a232168
vijai <vijai.djearam@gmail.com>
2021-02-19 14:04:59 +0100

    Update dellcommandupdate_applyupdates.ps1

--- dellcommandupdate_applyupdates.ps1
+++ dellcommandupdate_applyupdates.ps1
@@ -4,7 +4,7 @@ if($LASTEXITCODE -eq 0 -or $LASTEXITCODE -eq 1 -or $LASTEXITCODE -eq 5)
 {
 write-host "Stage: dellcommandupdate_applyupdates completed" -ForegroundColor Green
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'dellcommandconfigure'
-Set-Runonce -command "%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass ; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))"
+Set-Runonce
 Stop-Transcript
 Restart-Computer
 } 
@@ -12,7 +12,7 @@ else
 {
 write-host "Stage: dellcommandupdate_applyupdates Failed" -ForegroundColor Red
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value dellcommandupdate_applyupdates
-Set-Runonce -command "%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass ; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))"
+Set-Runonce
 Stop-Transcript
 Pause
 }
