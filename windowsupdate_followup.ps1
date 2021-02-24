108ed0cc4b1bc0edd9983b689dd894c697562024
vijai <vijai.djearam@gmail.com>
2021-02-24 12:30:42 +0100

    Update windowsupdate_followup.ps1

--- windowsupdate_followup.ps1
+++ windowsupdate_followup.ps1
@@ -8,7 +8,7 @@ if (Get-WURebootStatus -silent){
 write-host "Stage: windowsupdate_followup completed but requires a reboot , the system will reboot and check if there is any further windows update" -ForegroundColor Green
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'windowsupdate_followup'
  }
-else {Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'network_config'}
+else {Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'chocolatey_apps'}
 Set-Runonce
 Stop-Transcript
 Restart-Computer
