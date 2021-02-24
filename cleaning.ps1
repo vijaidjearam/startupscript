c9fdee3dbab9716166e367b0d9f7780b75370019
vijai <vijai.djearam@gmail.com>
2021-02-24 12:45:24 +0100

    Update cleaning.ps1

--- cleaning.ps1
+++ cleaning.ps1
@@ -1,9 +1,26 @@
+# Delete Temp Files
+Function DeleteTempFiles {
+    Write-Host "Cleaning up temporary files..."
+    $tempfolders = @("C:\Windows\Temp\*", "C:\Windows\Prefetch\*", "C:\Documents and Settings\*\Local Settings\temp\*", "C:\Users\*\Appdata\Local\Temp\*")
+    Remove-Item $tempfolders -force -recurse 2>&1 | Out-Null
+}
+
+# Clean WinSXS folder (WARNING: this takes a while!)
+Function CleanWinSXS {
+    Write-Host "Cleaning WinSXS folder, this may take a while, please wait..."
+    Dism.exe /online /Cleanup-Image /StartComponentCleanup
+}
+
 try
 {
 if (test-path C:\Windows\Setup\Scripts\SetupComplete.cmd){Remove-Item 'C:\Windows\Setup\Scripts\SetupComplete.cmd'}
 Write-host "Cleaning Event Log" 
 Get-EventLog -LogName * | ForEach { Clear-EventLog $_.Log }
 Write-host "Completed Cleaning Event Log" 
+iex CleanWinSXS
+Write-host "The Next step is going to clear Temp File, check the log file for any error message and then continue: "
+Pause
+iex DeleteTempFiles
 write-host "Stage: cleaning completed" -ForegroundColor Green
 Remove-Item -Path HKCU:\osinstall_local
 Stop-Transcript
