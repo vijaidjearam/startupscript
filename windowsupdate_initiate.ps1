fa2d3f16d309419e547259903fb58f4f37206d60
vijai <vijai.djearam@gmail.com>
2021-03-17 13:46:46 +0100

    Update windowsupdate_initiate.ps1

--- windowsupdate_initiate.ps1
+++ windowsupdate_initiate.ps1
@@ -3,17 +3,17 @@ Try
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSWindowsUpdate -Force
 Import-Module -Name PSWindowsUpdate
-Install-WindowsUpdate -AcceptAll -IgnoreReboot
-write-host "Stage: windowsupdate_initiate completed" -ForegroundColor Green
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'windowsupdate_followup'
 Set-Runonce
+Install-WindowsUpdate -AcceptAll -AutoReboot
+write-host "Stage: windowsupdate_initiate completed" -ForegroundColor Green
 Stop-Transcript
 Restart-Computer
 }
 catch
 {
 write-host "Stage: windowsupdate_initiate Failed" -ForegroundColor Red
-Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value windowsupdate_initiate
+Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value windowsupdate_followup
 Set-Runonce
 Stop-Transcript
 Pause
