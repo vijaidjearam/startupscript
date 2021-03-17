7d4c2f1ffb93e54c443d92e2f2c1ec1f2d483aed
vijai <vijai.djearam@gmail.com>
2021-03-17 13:53:10 +0100

    Update windowsupdate_followup.ps1

--- windowsupdate_followup.ps1
+++ windowsupdate_followup.ps1
@@ -1,14 +1,18 @@
 Try
 {
-
 Import-Module -Name PSWindowsUpdate
-Install-WindowsUpdate -AcceptAll -IgnoreReboot
-write-host "Stage: windowsupdate_followup completed" -ForegroundColor Green
-if (Get-WURebootStatus -silent){
-write-host "Stage: windowsupdate_followup completed but requires a reboot , the system will reboot and check if there is any further windows update" -ForegroundColor Green
+$temp = Get-WindowsUpdate
+if ($temp.Count -gt 0)
+{
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'windowsupdate_followup'
- }
-else {Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'windows_services'}
+Set-Runonce
+Install-WindowsUpdate -AcceptAll -AutoReboot
+write-host "Stage: windowsupdate_followup completed" -ForegroundColor Green
+Stop-Transcript
+Restart-Computer
+}
+else {
+Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'windows_services'
 Set-Runonce
 Stop-Transcript
 Restart-Computer
