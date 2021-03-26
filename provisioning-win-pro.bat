696465411a038a2f75dab8bda927e6293afa19f4
vijai <vijai.djearam@gmail.com>
2021-03-26 13:22:21 +0100

    Update provisioning-win-pro.bat

--- provisioning-win-pro.bat
+++ provisioning-win-pro.bat
@@ -3,7 +3,7 @@ set workdir=c:\temp\%date:~7,2%-%date:~-10,2%-%date:~-2,2%_%time:~0,2%.%time:~3,
 md %workdir%
 DISM /Mount-Wim /WimFile:%temp%\install.wim /index:1 /MountDir:%workdir%
 md %workdir%\Windows\Setup\Scripts
-echo reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" -v !test /t REG_SZ -d "cmd /k @powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command \" iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))\"" /f > %workdir%\Windows\Setup\Scripts\SetupComplete.cmd
+echo reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" -v test /t REG_SZ -d "cmd /k @powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command \" iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))\"" /f > %workdir%\Windows\Setup\Scripts\SetupComplete.cmd
 DISM /unmount-wim /MountDir:%workdir% /commit
 DEL %~dp0sources\install.esd
 Dism /Split-Image /ImageFile:%temp%\install.wim /SWMFile:%~dp0sources\install.wim /FileSize:3500
