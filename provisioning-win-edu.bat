e65e26a153f4d3c580e6835de4d4fcf9fd11b68c
vijai <vijai.djearam@gmail.com>
2021-04-02 16:10:03 +0200

    Update provisioning-win-edu.bat

--- provisioning-win-edu.bat
+++ provisioning-win-edu.bat
@@ -1,8 +1,8 @@
 dism /Export-Image /SourceImageFile:%~dp0sources\install.esd /SourceIndex:4 /DestinationImageFile:%temp%\install.wim /Compress:Max /CheckIntegrity
-SET HOUR=%time:~0,2%
-SET dtStamp9=%date:~-4%%date:~4,2%%date:~7,2%_0%time:~1,1%%time:~3,2%%time:~6,2% 
-SET dtStamp24=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
-if "%HOUR:~0,1%" == " " (SET workdir=%dtStamp9%) else (SET workdir=%dtStamp24%)
+set workdir=%date: =_%_%time: =0%
+set workdir=%workdir:/=_%
+set workdir=%workdir::=_%
+set workdir=%workdir:.=_%
 md %workdir%
 DISM /Mount-Wim /WimFile:%temp%\install.wim /index:1 /MountDir:%workdir%
 md %workdir%\Windows\Setup\Scripts
