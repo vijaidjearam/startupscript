f44fa70764c4b4259fade90a02a9d697ab4a3224
vijai <vijai.djearam@gmail.com>
2021-04-09 15:09:11 +0200

    Update windows_settings.ps1

--- windows_settings.ps1
+++ windows_settings.ps1
@@ -32,7 +32,6 @@ $setting = @(
 #A titre d’exemple de simplification de la UI, aller dans les paramètres avancés,sélectionner « Ajuster pour obtenir les meilleures performances pour lesprogrammes » et cochez dans la liste dessous « Afficher des miniatures au lieu d’icônes », ainsi que « Lisser les polices d’écran ».
 "performance_options_visual_effects",
 "pin-to-taskbar",
-"dontdisplaylastusername-on-logon"
 )
 
 $setting | foreach {
