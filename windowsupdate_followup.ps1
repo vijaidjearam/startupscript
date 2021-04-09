b84c4adfcd06496061ac2b3b0669a11a219bcb01
vijai <vijai.djearam@gmail.com>
2021-04-09 09:28:58 +0200

    the windows update followup stage will be done for a maximum of 5 iterations

--- windowsupdate_followup.ps1
+++ windowsupdate_followup.ps1
@@ -1,3 +1,5 @@
+function windowsupdatefollowup()
+{
 Try
 {
 Import-Module -Name PSWindowsUpdate
@@ -26,5 +28,56 @@ Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value windowsupdate_
 Set-Runonce
 Stop-Transcript
 Pause
+}
+}
+
+Function Test-RegistryValue($regkey, $name) 
+{
+    try
+    {
+        $exists = Get-ItemProperty $regkey $name -ErrorAction SilentlyContinue
+        Write-Host "Test-RegistryValue: $exists"
+        if (($exists -eq $null) -or ($exists.Length -eq 0))
+        {
+            return $false
+        }
+        else
+        {
+            return $true
+        }
+    }
+    catch
+    {
+        return $false
+    }
+}
+
+
+
+
+
+if(Test-RegistryValue ('HKCU:\osinstall_local','windowsupdateiteration'))
+{
+ $iter= Get-ItemPropertyValue -Path 'HKCU:\osinstall_local' -Name windowsupdateiteration
+ if ($iter -lt 5)
+ {
+ $iter = $iter + 1
+ Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name windowsupdateiteration -Value $iter -Force
+ windowsupdatefollowup
+ }
+ else
+ {
+  Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'windows_services'
+  Set-Runonce
+  Stop-Transcript
+  Restart-Computer 
+ }
+
+
+}
+else
+{
+New-ItemProperty -Path 'HKCU:\osinstall_local' -Name windowsupdateiteration -value 1
+windowsupdatefollowup
 }
 
