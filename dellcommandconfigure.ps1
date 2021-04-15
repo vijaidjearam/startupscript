1b535d96463712f2faf0afb13f0fa17c7f25a5b4
vijai <vijai.djearam@gmail.com>
2021-04-15 11:03:57 +0200

    Update dellcommandconfigure.ps1

--- dellcommandconfigure.ps1
+++ dellcommandconfigure.ps1
@@ -137,7 +137,7 @@ write-host "There was an error setting the option: $item " -ForegroundColor Red
 }
 }
 write-host "Stage: dellcommandconfigure completed" -ForegroundColor Green
-Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'windowsupdate_initiate'
+Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'chocolatey_apps'
 Set-Runonce
 Stop-Transcript
 Restart-Computer
