8e4e250ef73e323325f5a15f2e77dfc5ce7c0cd3
vijai <vijai.djearam@gmail.com>
2021-05-31 09:27:53 +0200

    Create google-chromepolicy-install.ps1

---
+++ google-chromepolicy-install.ps1
@@ -0,0 +1,2 @@
+((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/googlechrome-policy.ps1')) | Out-File $env:TEMP\googlechrome-policy.ps1
+$env:TEMP\googlechrome-policy.ps1 
