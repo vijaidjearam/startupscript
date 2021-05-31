d28248ff328630d5e6268ee47e02fdaa60453b05
vijai <vijai.djearam@gmail.com>
2021-05-31 09:44:15 +0200

    Update google-chromepolicy-install.ps1

--- google-chromepolicy-install.ps1
+++ google-chromepolicy-install.ps1
@@ -1,2 +1,3 @@
-((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/googlechrome-policy.ps1')) | Out-File '$env:TEMP\googlechrome-policy.ps1'
-./$env:TEMP\googlechrome-policy.ps1 
+((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/googlechrome-policy.ps1')) | Out-File $env:TEMP\googlechrome-policy.ps1
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+. $env:TEMP\googlechrome-policy.ps1 
