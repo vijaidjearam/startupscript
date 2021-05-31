befdc2bee4f8984582b4b1f33215fc5dd9a87729
vijai <vijai.djearam@gmail.com>
2021-05-31 10:08:56 +0200

    Delete google-chromepolicy-install.ps1

--- google-chromepolicy-install.ps1
+++
@@ -1,3 +0,0 @@
-((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/googlechrome-policy.ps1')) | Out-File $env:TEMP\googlechrome-policy.ps1
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force
-. $env:TEMP\googlechrome-policy.ps1 
