043b815bf7f75795d9216df34c0f1bf1a7f42936
vijai <vijai.djearam@gmail.com>
2021-08-30 16:09:46 +0200

    Update windows_settings.ps1

--- windows_settings.ps1
+++ windows_settings.ps1
@@ -31,6 +31,8 @@ $setting = @(
 "disable-scheduledtasks",
 #A titre d’exemple de simplification de la UI, aller dans les paramètres avancés,sélectionner « Ajuster pour obtenir les meilleures performances pour lesprogrammes » et cochez dans la liste dessous « Afficher des miniatures au lieu d’icônes », ainsi que « Lisser les polices d’écran ».
 "performance_options_visual_effects",
+#disable news and interests in taskbar
+"dontdisplaynewsaninterestsintaskbar",
 #pin office apps shortcut to taskbar
 "pin-to-taskbar",
 #Apply google chrome policy settings
