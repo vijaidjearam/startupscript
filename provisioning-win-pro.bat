36432cd91f21b9e79a2073dcd7a17489ae2d9d05
vijai <vijai.djearam@gmail.com>
2021-10-07 09:04:35 +0200

    Update provisioning-win-pro.bat

--- provisioning-win-pro.bat
+++ provisioning-win-pro.bat
@@ -9,6 +9,7 @@ md %workdir%
 DISM /Mount-Wim /WimFile:%temp%\install.wim /index:1 /MountDir:%workdir%
 md %workdir%\Windows\Setup\Scripts
 echo reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" -v test /t REG_SZ -d "cmd /k @powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command \" iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/header.ps1'))\"" /f > %workdir%\Windows\Setup\Scripts\SetupComplete.cmd
+DISM /Image:%workdir% /Add-Driver /Driver:C:\deploydriver /recurse
 DISM /unmount-wim /MountDir:%workdir% /commit
 DEL %~dp0sources\install.esd
 Dism /Split-Image /ImageFile:%temp%\install.wim /SWMFile:%~dp0sources\install.swm /FileSize:3500
