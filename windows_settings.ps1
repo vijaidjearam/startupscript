c1440dde9aba8bdf180b37c5f530889d5dba34e8
vijai <vijai.djearam@gmail.com>
2024-02-12 09:35:31 +0100

    Update windows_settings.ps1

--- windows_settings.ps1
+++ windows_settings.ps1
@@ -23,6 +23,7 @@ $setting = @(
 "set-desktop-icon-small",
 "Enable-RemoteDesktop",
 "open_ports",
+"Disable_automatic_updates_of_Microsoft_Store_apps",
 #Désactiver la protection du système (points de restauration) : nous gagnons alors de l’espace disque. Nous avons une image qui fonctionne désormais, et il n’est pas nécessaire de conserver des points de restauration.
 'Disable-ComputerRestore -Drive "c:\"',
 #point windows wsus server to 10.1.1.1( its a dummy address so windows will get updates anymore)
