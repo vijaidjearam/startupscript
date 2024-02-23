5c21512eae09da9c16201b70244e6b09866195b4
vijai <vijai.djearam@gmail.com>
2024-02-23 13:54:07 +0100

    Update registry_run_once_install_win_with_recovery.ps1

--- registry_run_once_install_win_with_recovery.ps1
+++ registry_run_once_install_win_with_recovery.ps1
@@ -3,4 +3,12 @@ while (!(test-connection 8.8.8.8 -Count 1 -Quiet -ErrorAction SilentlyContinue))
     sleep 5
 }
 write-host "internet connection is up! -> proceeding with the script" -ForegroundColor Green
-iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vijaidjearam/startupscript/master/install_windows_with_recovery_partition.ps1'))
+$repopath ="https://raw.githubusercontent.com/vijaidjearam/startupscript/master/"
+iex ((New-Object System.Net.WebClient).DownloadString($repopath+'install_windows_with_recovery_partition.ps1'))
+if (test-path 'HKCU:\repopath'){
+$repopath = Get-ItemPropertyValue -Path 'HKCU:\repopath' -Name path
+}
+else{
+New-ItemProperty -Path 'HKCU:\repopath' -Name path -value $repopath
+}
+
