8ef54b9d32cf205bc5fe8017cf7d225f8ee7ef31
vijai <vijai.djearam@gmail.com>
2024-02-23 13:07:54 +0100

    Update registryrunonce.ps1

--- registryrunonce.ps1
+++ registryrunonce.ps1
@@ -3,5 +3,5 @@ while (!(test-connection 8.8.8.8 -Count 1 -Quiet -ErrorAction SilentlyContinue))
     sleep 5
 }
 write-host "internet connection is up! -> proceeding with the script" -ForegroundColor Green
-$repopath = Get-ItemPropertyValue -Path 'HKCU:\osinstall_local' -Name repopath
+$repopath ="https://raw.githubusercontent.com/vijaidjearam/startupscript/master/"
 iex ((New-Object System.Net.WebClient).DownloadString($repopath+'header.ps1'))
