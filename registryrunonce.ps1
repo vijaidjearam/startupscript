283d1fbdadfbff27ec33f5be38f1dc576acdbfc1
vijai <vijai.djearam@gmail.com>
2024-02-23 13:50:24 +0100

    Update registryrunonce.ps1

--- registryrunonce.ps1
+++ registryrunonce.ps1
@@ -5,3 +5,9 @@ while (!(test-connection 8.8.8.8 -Count 1 -Quiet -ErrorAction SilentlyContinue))
 write-host "internet connection is up! -> proceeding with the script" -ForegroundColor Green
 $repopath ="https://raw.githubusercontent.com/vijaidjearam/startupscript/master/"
 iex ((New-Object System.Net.WebClient).DownloadString($repopath+'header.ps1'))
+if (test-path 'HKCU:\repopath'){
+$repopath = Get-ItemPropertyValue -Path 'HKCU:\repopath' -Name path
+}
+else{
+New-ItemProperty -Path 'HKCU:\repopath' -Name path -value $repopath
+}
