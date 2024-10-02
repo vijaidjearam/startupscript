22cb4e1832563ec6dc9b06c3bdd123e63eafc35f
vijai <vijai.djearam@gmail.com>
2024-10-02 15:43:49 +0200

    Update hpia_install.ps1

--- hpia_install.ps1
+++ hpia_install.ps1
@@ -16,7 +16,7 @@ while (-not $resolved) {
 choco install hpia -y
 if($LASTEXITCODE -eq 0 -or $LASTEXITCODE -eq 1 -or $LASTEXITCODE -eq 5 -or $LASTEXITCODE -eq 500)
 {
-write-host "Stage: dellcommandupdate_driverinstall completed" -ForegroundColor Green
+write-host "Stage: HP Image Assist installation completed" -ForegroundColor Green
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'Hpia_driverinstall'
 Set-Runonce
 Stop-Transcript
@@ -24,7 +24,7 @@ Restart-Computer
 } 
 else 
 {
-write-host "Stage: dellcommandupdate_driverinstall Failed" -ForegroundColor Red
+write-host "Stage: HP image assist installation Failed" -ForegroundColor Red
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'hpia_install'
 Set-Runonce
 Stop-Transcript
