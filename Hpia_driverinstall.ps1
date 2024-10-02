a7abab8262003a44c545c81a678cb05a8312f92d
vijai <vijai.djearam@gmail.com>
2024-10-02 17:35:01 +0200

    Update Hpia_driverinstall.ps1

--- Hpia_driverinstall.ps1
+++ Hpia_driverinstall.ps1
@@ -1,6 +1,6 @@
-start-process -FilePath c:\hp\hpia\Hpimageassistant.exe -ArgumentList "/auto /Noninteractive /AutoCleanup" -wait
+$proc = (start-process -FilePath c:\hp\hpia\Hpimageassistant.exe -ArgumentList "/auto /Noninteractive /AutoCleanup" -wait -PassThru)
 
-if($LASTEXITCODE -eq 0 -or $LASTEXITCODE -eq 1 -or $LASTEXITCODE -eq 5 -or $LASTEXITCODE -eq 500)
+if($proc.ExitCode -eq 0 -or $proc.ExitCode -eq 256 -or $proc.ExitCode -eq 257 -or $proc.ExitCode -eq 3010)
 {
 write-host "Stage: HP Image Assist -Driver Install completed" -ForegroundColor Green
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'chocolatey_apps'
