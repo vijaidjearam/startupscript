98affffc57850eb000972c323f817902a8084953
vijai <vijai.djearam@gmail.com>
2024-10-03 08:08:19 +0200

    Update Hpia_driverinstall.ps1

--- Hpia_driverinstall.ps1
+++ Hpia_driverinstall.ps1
@@ -1,4 +1,5 @@
 $proc = (start-process -FilePath c:\hp\hpia\Hpimageassistant.exe -ArgumentList "/auto /Noninteractive /AutoCleanup" -wait -PassThru)
+write-host "Lastexitcode : $proc.ExitCode"
 
 if($proc.ExitCode -eq 0 -or $proc.ExitCode -eq 256 -or $proc.ExitCode -eq 257 -or $proc.ExitCode -eq 3010)
 {
