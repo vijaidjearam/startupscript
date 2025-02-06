e9e91dda1b86d9d3c3ea4fca305344dd0931ae0f
vijai <vijai.djearam@gmail.com>
2025-02-06 13:50:46 +0100

    Update windows_settings.ps1

--- windows_settings.ps1
+++ windows_settings.ps1
@@ -42,8 +42,6 @@ $setting = @(
 "pin-to-taskbar",
 #Apply google chrome policy settings
 "googlechrome-policy",
-#Dont display last username on logon
-"dontdisplaylastusername-on-logon",
 # Disable Google Chrome Turn on ad privacy feature, source: https://github.com/letsdoautomation/powershell
 "DisableGoogleChromeTurnOnAdPrivacyFeature",
 #Disable Microsoft Edge first run wizard
